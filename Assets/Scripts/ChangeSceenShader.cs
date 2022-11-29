5419869e375cae8008fd7ebc18f0133a109d8e9f
bbobbii <justghuys@gmail.com>
2022-11-29 16:39:38 +0100

    Worked on the screen shader
    added screen shander, started working on how to modify it with script

---
+++ Assets/Scripts/ChangeSceenShader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceenShader : MonoBehaviour
+{
+    public Material material;
+  /*  [SerializeField] private float blinkFrequency = 2f;
+    Material material;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        white = SharderGraphs.Load<Material>("Material/Material_Fullscreen_Test");
+        red = SharderGraphs.Load<Material>("Material/Red");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = 1 / blinkFrequency;
+
+            blinkObject = !blinkObject;
+            foreach (var child in rend_ThisObject)
+            {
+                child.material = blinkObject ? white : red;
+            }
+        }
+    }*/
+}
