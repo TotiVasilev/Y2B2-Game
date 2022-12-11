7719643c1f8df77b2ae8869df1043cb4fb892a02
Hufflepack <106102230+Hufflepack@users.noreply.github.com>
2022-12-11 01:18:54 +0100

    UI stuff

---
+++ Assets/Scenes/UI Gameview/TimeManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TimeManager : MonoBehaviour
+{
+    public static Action OnMinuteChanged;
+    public static Action OnHourChanged;
+
+    public static int Minute { get; private set; }
+
+    public static int Hour { get; private set; }
+
+    private float minuteToRealTime = 3.0F;
+    private float timer;
+
+
+
+    void Start()
+    {
+
+        Minute = 0;
+        Hour = 18;
+        timer = minuteToRealTime;
+
+
+
+    }
+
+    void Update()
+    {
+
+
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+
+            Minute++;
+            OnMinuteChanged?.Invoke();
+
+            if(Minute >= 60)
+            {
+
+                Hour++;
+                Minute = 0;
+                OnHourChanged?.Invoke();
+
+
+            }
+
+            timer = minuteToRealTime;
+
+        }
+
+    }
+
+}
