1769219da7ca32843357e240033b007353072bc1
Hufflepack <106102230+Hufflepack@users.noreply.github.com>
2022-12-11 01:37:42 +0100

    More UI

--- Assets/Scenes/UI Gameview/Timer.cs
+++ Assets/Scenes/UI Gameview/Timer.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    float currentTime;
+    public float startingTime = 10f;
+
+    [SerializeField] Text countdownText;
+
     void Start()
     {
-        
+
+        currentTime = startingTime;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void update()
     {
-        
+
+        currentTime += 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime >= 0)
+        {
+
+            currentTime = 0;
+
+            
+
+        }
+
     }
+
 }
