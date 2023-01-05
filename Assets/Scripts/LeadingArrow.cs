dcffcebfb87a8b402e624395f58df07285ee09d7
bbobbii <justghuys@gmail.com>
2023-01-05 12:58:00 +0100

    made the floor arrow blink:)

--- Assets/Scripts/LeadingArrow.cs
+++ Assets/Scripts/LeadingArrow.cs
@@ -9,6 +9,10 @@ public class LeadingArrow : MonoBehaviour
     GameObject player;
     Renderer rend;
 
+    float fullGlow = 60;
+    public float lightTime = 10;
+    float glow;
+
     //public float temperature;
     public float origin;
 
@@ -19,17 +23,47 @@ public class LeadingArrow : MonoBehaviour
 
         rend = GetComponent<Renderer>();
         rend.material = new Material(rend.material);
+        glow = fullGlow;
+        rend.material.SetFloat("_Glow", glow);
+        StartCoroutine(EmissionUp());
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float dist = Vector3.Distance(player.transform.position, endOfArrow.transform.position);
-     
+
+        //float glow = Time.deltaTime * dist;     
+
         //temperature = - dist + 30;
         origin = - dist + 5 ;
 
         rend.material.SetFloat("_Origin", origin);
+        rend.material.SetFloat("_Glow", glow);
         //rend.material.SetFloat("_Temperature", temperature);
+    }
+
+
+    // Coroutines that make the emmision go up and down constantly/repeatedly 
+    IEnumerator EmissionUp()
+    {
+        while (glow > 0){
+            glow -= lightTime * Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(EmissionDown());
+    }
+
+    IEnumerator EmissionDown()
+    {
+        while (glow < 60)
+        {
+            glow += lightTime * Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(EmissionUp());
     }
 }
