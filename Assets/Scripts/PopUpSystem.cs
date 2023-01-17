b0e893c2759f5df09cfd3b6b373debe5f5a874af
Hufflepack <106102230+Hufflepack@users.noreply.github.com>
2023-01-17 23:09:11 +0100

    Pop up box

--- Assets/Scripts/PopUpSystem.cs
+++ Assets/Scripts/PopUpSystem.cs
@@ -3,20 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class PopUpSystem : MonoBehaviour
+public class PopUpWindow : MonoBehaviour
 {
-    public GameObject PopUpBox;
-    public Animator animator;
-    public TMP_Text popUpText;
 
-    public void PopUp(string text)
+    public TMP_Text popupText;
+
+    private GameObject window;
+    private Animator popupAnimator;
+
+        private Queue<string> popupQueue;
+    private bool IsActive;
+    private Coroutine queueChecker;
+
+
+    public void Start()
     {
 
-        PopUpBox.SetActive(true);
-        popUpText.text = text;
-        animator.SetTrigger("Watch out! Tempature in the room is too hot");
+        window = transform.GetChild(0).gameObject;
+            popupAnimator = window.GetComponent<Animator>();
+        window.SetActive(false);
+        popupQueue = new Queue<string>();
 
     }
+
+    public void AddToQueu(string text)
+    {
+        popupQueue.Enqueue(text);
+        if (queueChecker == null)
+            queueChecker = StartCoroutine(checkQueue());
+
+    }
+
+
+    private void ShowPopup(string text)
+    {
+
+        IsActive = true;
+        window.SetActive(true);
+        popupText.text = text;
+        popupAnimator.Play("PopupAnimation");
+
+    }
+
+    private IEnumerator CheckQueu()
+    {
+        do
+        {
+
+            ShowPopup(popupQueue.Dequeue());
+           do {
+
+             yield return null;
+
+            } while (!popupAnimator.GetCurrentAnimatorStateInfo(0).IsTag("idle"));
+
+
+        } while (popupQueue.Count > 0);
+        IsActive = false;
+        window.SetActive(false);
+        queueChecker = 0;
+
+
+
+
+    }
+
+
+
 }
+
+
+
+   
 
 
